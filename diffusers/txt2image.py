7bdb4f70f196aa190adfc7487a8f16a52afc06ee
Lukas Steinwender <lukas.steinwender@gmx.de>
2023-03-17 14:19:13 +0100

    updated diffusers

--- diffusers/txt2image.py
+++ diffusers/txt2image.py
@@ -1,5 +1,20 @@
 from diffusers import DiffusionPipeline
+import torch
 
-pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
-pipeline.to("cuda")
-pipeline("An image of a squirrel in Picasso style").images[0]
+torch.cuda.empty_cache()
+
+
+pipe = DiffusionPipeline.from_pretrained(
+    "SG161222/Realistic_Vision_V1.4_Fantasy.ai",
+    
+    torch_dtype=torch.float32,
+)
+pipe = pipe.to("cuda")
+
+def dummy(images, **kwargs):
+    return images, False
+pipe.safety_checker = dummy
+
+prompt = "a photo of an astronaut riding a horse on mars"
+image = pipe(prompt).images[0]
+image.save("output.jpg")
