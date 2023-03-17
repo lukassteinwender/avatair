1023aed1ab2a93a63f1ba75f09d4098bf7b3ce98
Lukas Steinwender <lukas.steinwender@gmx.de>
2023-03-17 14:54:52 +0100

    added negative prompt flag

--- interface.py
+++ interface.py
@@ -8,7 +8,7 @@ def diffusion(prompt):
 
     pipe = DiffusionPipeline.from_pretrained(
         "SG161222/Realistic_Vision_V1.4_Fantasy.ai",
-    
+        negative_prompt="(deformed iris, deformed pupils, semi-realistic, cgi, 3d, render, sketch, cartoon, anime:1.4), text, close up, cropped, out of frame, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck",
         torch_dtype=torch.float32,
     )
     pipe = pipe.to("cuda")
