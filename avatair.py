f7c01abc10eafad3331035a438516f88eb266eaa
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2023-11-29 09:47:54 +0100

    Update avatair.py

--- avatair.py
+++ avatair.py
@@ -768,7 +768,7 @@ def main():
             torch.manual_seed(random.randint(0, 1000))
             
             model_id = config.model
-
+            steps=20
             if(model_id == "SG161222/Realistic_Vision_V1.4"):
                 pipe = DiffusionPipeline.from_pretrained(
                     model_id,
@@ -786,6 +786,8 @@ def main():
             if(model_id == "stabilityai/sdxl-turbo"):
                 pipe = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16", use_safetensors=True)
                 #pipe.to("cuda")
+                # turbo only requires one step
+                steps=1
                 pipe.enable_model_cpu_offload()
                 
             global INITIAL_CHECK
@@ -793,9 +795,9 @@ def main():
             
 
             if(config.pictures == 1):
-                image = pipe(prompt=prompt, negative_prompt=negative_prompt, width=512, height=512).images[0]
+                image = pipe(prompt=prompt, negative_prompt=negative_prompt, num_inference_steps=steps, width=512, height=512).images[0]
             else:
-                images = pipe(prompt=[prompt] * config.pictures, negative_prompt=[negative_prompt] * config.pictures, width=512, height=512).images
+                images = pipe(prompt=[prompt] * config.pictures, negative_prompt=[negative_prompt] * config.pictures, num_inference_steps=steps, width=512, height=512).images
                 grid = scripts.image_grid(images, rows=1, cols=config.pictures)
                 image = grid
 
