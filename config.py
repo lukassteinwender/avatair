7416c17926786c82a9d61dc0dae2e66c21a94879
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2023-11-29 10:12:55 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@
 # -------------------------------------------------------------------------------------------
 
 # Number of iterations that will run until the program is finished:
-initial = 5
+initial = 20
 
 # Which scales should be used ( 1 = acceptance, likeability, empathy, anthropomorphism, trust | 2 = openness, conscientiousness, extraversion, agreeableness, neuroticism | 3 = efficiency )
 scales = 1
 
-# How much pictures are generated during one iteration
+# How many pictures are generated during one iteration
 pictures = 1
 
-# After how much iterations should enter the attention check ? (Array (e.g [1,2,3]), [-1] for no check)
+# After how many iterations should the attention check be entered? (Array (e.g., [1,2,3]), [-1] for no check)
 attention = [1,3]
 
 # Diffusers - Model ( RV: "SG161222/Realistic_Vision_V1.4" )
 model = "SG161222/Realistic_Vision_V1.4"
 
-# Your token for the huggingface-login (needed for some models, if not needed leave as "")
+# Your token for the huggingface-login (needed for some models. If not needed, leave as "")
 token = ""
 
 # The promptmodel that will be used for the model ("defined" or "latent")
