e55d86677ac4cad6b680ebd762723bfe6183f246
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2026-05-22 14:18:14 +0100

    Refactor closest_colour function to use name_to_rgb

--- color.py
+++ color.py
@@ -10,13 +10,13 @@ def colour_to_text(colour_val):
 
 def closest_colour(requested_colour): 
     min_colours = {}
-    for key, name in webcolors.CSS3_HEX_TO_NAMES.items():
-        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
-        rd = (r_c - requested_colour[0]) ** 2
-        gd = (g_c - requested_colour[1]) ** 2
-        bd = (b_c - requested_colour[2]) ** 2
-        min_colours[(rd + gd + bd)] = name
-    return min_colours[min(min_colours.keys())]
+    for name in webcolors.names("css3"):
+        r_c, g_c, b_c = webcolors.name_to_rgb(name, spec="css3")
+        rd = (r_c - requested_color[0]) ** 2
+        gd = (g_c - requested_color[1]) ** 2
+        bd = (b_c - requested_color[2]) ** 2
+        min_colors[(rd + gd + bd)] = name
+    return min_colors[min(min_colors.keys())]
 
 
 
